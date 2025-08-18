01444cd6f2caabec84a0a1e4bec926c2b8db6a98
Mateus Bergone de Faria <bergone23@gmail.com>
2025-08-18 13:59:47 +0000

    Criei o printf e scanf afim de tornar o jogo interativo.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("CartasSuperTrunfo\n\n");
+    //Carta 1
+    int Populacao, NumeroDePontosTuristicos;
+    float Area, PIB;
 
+    printf("Carta 1\n\n");
+
+    printf("Qual a população da cidade?\n");
+    scanf("%d", &Populacao);
+
+    printf("Qual a área da cidade?\n");
+    scanf("%f", &Area);
+
+    printf("Qual o PIB da cidade?\n");
+    scanf("%f", &PIB);
+
+    printf("Quantos pontos turísticos a cidade tem?\n");
+    scanf("%d", &NumeroDePontosTuristicos);
+    printf("\n");
+
+//Carta 2
+    int Populacao2, NumeroDePontosTuristicos2;
+    float Area2, PIB2;
+
+    printf("Carta 2\n\n");
+
+    printf("Qual a população da cidade?\n");
+    scanf("%d", &Populacao2);
+
+    printf("Qual a área da cidade?\n");
+    scanf("%f", &Area2);
+
+    printf("Qual o PIB da cidade?\n");
+    scanf("%f", &PIB2);
+
+    printf("Quantos pontos turísticos a cidade tem?\n");
+    scanf("%d", &NumeroDePontosTuristicos2);
+    printf("\n");
     return 0;
 }
